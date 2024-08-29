7492c5200ba37fd8a764a316ee11995cb804b95b
ehsanhasan <mahmudulhasanehsan@gmail.com>
2024-08-29 21:26:35 +0600

    add to the cart complite

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 // middleWare 
 app.use(cors())
@@ -27,51 +27,61 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
+        const brandCollection = client.db('foodixDb').collection('brand')
+        const categoryCollection = client.db('foodixDb').collection('category')
+        const addProductCollection = client.db('foodixDb').collection('addProduct')
+        const cartsCollection = client.db('foodixDb').collection('carts')
 
-        const foodsCollection = client.db('foodsDB').collection('food')
-        const cartCollection = client.db('foodsDB').collection('carts')
-        const productCollection = client.db('foodsDB').collection('products')
-        const foodCategoryCollection = client.db('foodsDB').collection('foodCategory')
 
 
-        // product 
-        app.get('/brand/products', async (req, res) => {
-            const result = await productCollection.find().toArray()
+        // brand collection api
+        app.get('/read-brand', async (req, res) => {
+            const result = await brandCollection.find().toArray()
             res.send(result)
         })
 
+        // category collection api
 
-        // user addItem 
-        app.get('/addedProduct', async (req, res) => {
-            const cursor = await foodsCollection.find().toArray()
-            res.send(cursor)
+        app.get('/read-category', async (req, res) => {
+            const result = await categoryCollection.find().toArray()
+            res.send(result)
         })
 
-        app.get('/addedProduct/:id', async (req, res) => {
+        app.get('/read-category/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
-            const result = await foodsCollection.findOne(query)
-            res.send(result)
-        })
-
-        app.post('/addedProduct', async (req, res) => {
-            const newFood = req.body;
-            console.log(newFood);
-            const result = await foodsCollection.insertOne(newFood)
-            res.send(result)
-        })
-
-        // add to cart 
-        app.post('/carts', async (req, res) => {
-            const newCart = req.body;
-            console.log(newCart);
-            const result = await cartCollection.insertOne(newCart)
+            const result = await categoryCollection.findOne(query)
             res.send(result)
         })
 
 
+        // cart related api
+        app.get('/read-carts', async (req, res) => {
+            const email = req.query.email;
+            // console.log('email', email);
+            let query = {}
+            if (email) {
+                query = { email: email }
+            }
+            console.log(query);
+            const result = await cartsCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        app.post('/added-carts', async (req, res) => {
+            const product = req.body;
+            console.log('added product', product);
+            const result = await cartsCollection.insertOne(product)
+            res.send(result)
+
+        })
 
 
+        // all product api
+        app.get('/allProduct', async (req, res) => {
+            const result = await categoryCollection.find().toArray()
+            res.send(result)
+        })
 
 
         // Send a ping to confirm a successful connection
@@ -88,7 +98,7 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('server site is running')
+    res.send('foodix site is running')
 })
 
 app.listen(port, () => {
